26e7e4a35a9a57eb2317db9e315f77aeefdf7c9d
Reccy <aaronmeaney@hotmail.com>
2017-11-19 21:43:51 +0000

    Added renderer for coordinates.

---
+++ Assets/Scripts/CoordinateRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Mapbox.Unity.Location;
+using UnityEngine.UI;
+using UnityEngine;
+
+/**
+ * Renders the coordinates to the UI
+ */
+public class CoordinateRenderer : MonoBehaviour {
+    private TransformLocationProvider locationProvider;
+    public Text textUI;
+
+    // Get the component on the game object
+    void Awake()
+    {
+        locationProvider = GetComponent<TransformLocationProvider>();
+    }
+
+    // Print location every frame
+    void Update()
+    {
+        textUI.text = "Latitude: " + locationProvider.Location.x + "\nLongitude: " + locationProvider.Location.y;
+    }
+
+}

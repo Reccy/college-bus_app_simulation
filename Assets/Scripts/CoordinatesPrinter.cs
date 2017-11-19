b6b85ab76bb4b99155eefffae362f1cc6995ae2a
Reccy <aaronmeaney@hotmail.com>
2017-11-19 21:28:37 +0000

    Added debug printer for the FlyCams coordinates

---
+++ Assets/Scripts/CoordinatesPrinter.cs
@@ -0,0 +1,21 @@
+﻿using Mapbox.Unity.Location;
+using UnityEngine;
+
+/**
+ * Prints the co-ordinates of the game object
+ */
+public class CoordinatesPrinter : MonoBehaviour {
+    public TransformLocationProvider locationProvider;
+
+    // Get the component on the game object
+    void Awake ()
+    {
+        locationProvider = GetComponent<TransformLocationProvider>();
+    }
+
+    // Print location every frame
+	void Update () {
+        Debug.Log(gameObject.name + " coordinates: " + locationProvider.Location);
+	}
+
+}

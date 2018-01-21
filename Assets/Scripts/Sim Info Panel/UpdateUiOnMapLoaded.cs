47604b91a6f0aa3ec550c3f0775d77b239fde706
Reccy <aaronmeaney@hotmail.com>
2018-01-21 16:44:53 +0000

    Working on updating ui

---
+++ Assets/Scripts/Sim Info Panel/UpdateUiOnMapLoaded.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Mapbox.Unity.Map;
+
+// Copied from Mapbox example code:
+// Mapbox\Examples\Scripts\LoadingPanelController.cs
+public class UpdateUiOnMapLoaded : MonoBehaviour {
+    public MapVisualizer MapVisualizer;
+    public GameObject SetInvisible;
+    public GameObject SetVisible;
+
+    void Awake()
+    {
+        SetInvisible.SetActive(true);
+        SetVisible.SetActive(false);
+
+        MapVisualizer.OnMapVisualizerStateChanged += (s) =>
+        {
+            if (s == ModuleState.Finished)
+            {
+                SetInvisible.SetActive(false);
+                SetVisible.SetActive(true);
+            }
+            else if (s == ModuleState.Working)
+            {
+                SetInvisible.SetActive(true);
+                SetVisible.SetActive(false);
+            }
+
+        };
+    }
+}

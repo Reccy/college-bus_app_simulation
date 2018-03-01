9c1cd9350dd3a20c52c5e1c9fc17344a8d61b107
Aaron Meaney <aaronmeaney@hotmail.com>
2018-03-01 19:53:25 +0000

    Implemented DateTime Manager

---
+++ Assets/Scripts/Core/DateTimeManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace AaronMeaney.BusStop.Core
+{
+    /// <summary>
+    /// Responsible for maintaining the current date/time in the simulation
+    /// </summary>
+    public class DateTimeManager : MonoBehaviour
+    {
+        /// <summary>
+        /// The DateTime determined by the user/simulation.
+        /// </summary>
+        private DateTime currentDateTime;
+
+        /// <summary>
+        /// The current date time of the simulation
+        /// </summary>
+        public DateTime CurrentDateTime {
+            get
+            {
+                return currentDateTime;
+            }
+            set
+            {
+                currentDateTime = value;
+            }
+        }
+        
+        /// <summary>
+        /// Sets the current date time to now.
+        /// </summary>
+        public void SetToNow()
+        {
+            CurrentDateTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Adds the deltaTime to the current time, keeping it up to date.
+        /// </summary>
+        public void AdvanceTimeByDeltaTime()
+        {
+            CurrentDateTime = CurrentDateTime.AddSeconds(Time.deltaTime);
+        }
+
+        private void Awake()
+        {
+            SetToNow();
+        }
+
+        private void Update()
+        {
+            AdvanceTimeByDeltaTime();
+        }
+    }
+}

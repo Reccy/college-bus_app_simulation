678d0dd5e3ccdaa45f905130c3d90aa49b86de80
Aaron Meaney <aaronmeaney@hotmail.com>
2018-03-02 13:41:57 +0000

    Moved BusRouteNode to its own C-Sharp file

--- Assets/Scripts/Core/BusRoute.cs
+++ Assets/Scripts/Core/BusRoute.cs
@@ -91,42 +91,4 @@ namespace AaronMeaney.BusStop.Core
                 onBusRoutePopulated(this);
         }
     }
-
-    /// <summary>
-    /// Represents the latitude, longitude and Unity world position of a bus route node.
-    /// </summary>
-    [Serializable]
-    public class BusRouteNode
-    {
-        [SerializeField]
-        private float latitude;
-        public float Latitude { get { return latitude; } }
-
-        [SerializeField]
-        private float longitude;
-        public float Longitude { get { return longitude; } }
-
-        public BusRouteNode(float latitude, float longitude)
-        {
-            this.latitude = latitude;
-            this.longitude = longitude;
-        }
-
-        public BusRouteNode(Vector2 latLong)
-        {
-            this.latitude = latLong.x;
-            this.longitude = latLong.y;
-        }
-
-        /// <summary>
-        /// The Bus Route Node as a Unity world position.
-        /// </summary>
-        /// <param name="map">The map to reference the latitude/longitude against to translate it to Unity world coordinated.</param>
-        /// <returns>Position of the node in the world.</returns>
-        public Vector3 AsUnityPosition(AbstractMap map)
-        {
-            Vector3 unityPosition = new Vector2(latitude, longitude).AsUnityPosition(map.CenterMercator, map.WorldRelativeScale);
-            return unityPosition;
-        }
-    }
 }
---
+++ Assets/Scripts/Core/BusRouteNode.cs
@@ -0,0 +1,44 @@
+﻿using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
+using UnityEngine;
+
+namespace AaronMeaney.BusStop.Core
+{
+    /// <summary>
+    /// Represents the latitude, longitude and Unity world position of a bus route node.
+    /// </summary>
+    [System.Serializable]
+    public class BusRouteNode
+    {
+        [SerializeField]
+        private float latitude;
+        public float Latitude { get { return latitude; } }
+
+        [SerializeField]
+        private float longitude;
+        public float Longitude { get { return longitude; } }
+
+        public BusRouteNode(float latitude, float longitude)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+        }
+
+        public BusRouteNode(Vector2 latLong)
+        {
+            this.latitude = latLong.x;
+            this.longitude = latLong.y;
+        }
+
+        /// <summary>
+        /// The Bus Route Node as a Unity world position.
+        /// </summary>
+        /// <param name="map">The map to reference the latitude/longitude against to translate it to Unity world coordinated.</param>
+        /// <returns>Position of the node in the world.</returns>
+        public Vector3 AsUnityPosition(AbstractMap map)
+        {
+            Vector3 unityPosition = new Vector2(latitude, longitude).AsUnityPosition(map.CenterMercator, map.WorldRelativeScale);
+            return unityPosition;
+        }
+    }
+}
